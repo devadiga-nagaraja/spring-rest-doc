7f925ac1c6935eec5f638bda2e293ce656507a9c
NAGARAJA DEVADIGA <nagaraja.devadiga@espn.com>
2020-04-18 11:29:20 +0530

    First commit

---
+++ src/test/java/com/example/srd/SpringRestDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.srd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
